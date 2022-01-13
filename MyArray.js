a834e8caa3b14bca1098c4306ca45e0e0e32ee7f
Kova <frits1087@gmail.com>
2022-01-13 12:28:42 +0200

    FIle +

--- MyArray.js
+++ MyArray.js
@@ -1,1 +1,30 @@
+function MyArray(){
+    this.length = 0;
+    this.arguments = function(){
+
+    }
+
+
+function MyProtoArray(){
+    this.push = function(item) {
+        this[this.item] = item;
+        return ++this.length;
+    }
+
+    this.pop = function(){
+        if(this.length === 0){
+        return
+    }
+
+    const delItem = this[this.length - 1];
+    delete this[--this.length];
+    
+    return delItem;
+    }
+
+
+} 
+}
+MyArray.prototype = new MyProtoArray(); // связь с прототипом
+
  
